5e183fce18a77c4ca7f2f926167c566dd7d0b83e
ichiro-ss <sei.swmr@gmail.com>
2024-01-09 18:56:04 +0900

    handlefunc

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	h1 := func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintln(w, "Hello World!")
+	}
+
+	http.HandleFunc("/", h1)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
