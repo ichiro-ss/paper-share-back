3813c4bba2b5ef2a756225978b1ea32a2b2cdb87
ichiro-ss <sei.swmr@gmail.com>
2024-01-27 15:26:38 +0900

    db initialized

--- data/sqls/init.sql
+++ data/sqls/init.sql
@@ -1,12 +1,12 @@
-USE paper_share;
+USE share_database;
 
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     createdAt DATETIME,
     upcatedAt DATETIME,
-    deletedAt DATETIME,
-)
+    deletedAt DATETIME
+);
 
 CREATE TABLE authorization (
     loginId VARCHAR(100) PRIMARY KEY,
@@ -14,26 +14,26 @@ CREATE TABLE authorization (
     password VARCHAR(20) NOT NULL,
     createdAt DATETIME,
     updatedAt DATETIME,
-    FOREIGN KEY fk_auth_userId(userId) REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE,
-)
+    FOREIGN KEY fk_auth_userId(userId) REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
 
-create TABLE summary (
+CREATE TABLE summary (
     id INT AUTO_INCREMENT PRIMARY KEY,
     userId INT NOT NULL,
     title VARCHAR(50) NOT NULL,
     markdown TEXT NOT NULL,
-    FOREIGN KEY fk_summary_userId(userId) REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE,
-)
+    FOREIGN KEY fk_summary_userId(userId) REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
 
-create TABLE paper_authors (
+CREATE TABLE paper_authors (
     id INT AUTO_INCREMENT PRIMARY KEY,
     paperId INT NOT NULL,
     userId INT NOT NULL,
-    FOREIGN KEY fk_pa_paperId(paperId) REFERENCES paper(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY fk_pa_userId(userId) REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE,
-)
+    FOREIGN KEY fk_pa_paperId(paperId) REFERENCES summary(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY fk_pa_userId(userId) REFERENCES user(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
 
-create TABLE author (
+CREATE TABLE author (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-)
+    name VARCHAR(50) NOT NULL
+);
