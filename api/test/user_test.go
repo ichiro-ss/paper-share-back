63b7d6d996f1c8fff39d347cb3769826c8b6b929
ichiro-ss <sei.swmr@gmail.com>
2024-02-04 04:42:49 +0900

    user test added

--- api/test/user_test.go
+++ api/test/user_test.go
@@ -10,7 +10,22 @@ func TestUser(t *testing.T) {
 	name := "name"
 	user := data.User{Id: id, Name: name}
 
+	// test for creating user
 	if err := user.Create(); err != nil {
+		t.Error(err)
+		return
+	}
+
+	// test for getting user
+	if test_user, err := data.Get(id); err != nil {
+		t.Error(err)
+		return
+	}
+
+	//test for updating user
+
+	//test for deleting user
+	if err = data.Delete(id); err != nil {
 		t.Error(err)
 		return
 	}
