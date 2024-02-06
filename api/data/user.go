6295ce5daec4d47d0c5b5039a042ff1807024cc8
ichiro-ss <sei.swmr@gmail.com>
2024-02-06 20:56:52 +0900

    user create modefied

--- api/data/user.go
+++ api/data/user.go
@@ -2,30 +2,48 @@ package data
 
 import (
 	"fmt"
-	"time"
 )
 
-var tableName = "user"
-var idCol = "id"
+var tableUser = "user"
 var nameCol = "name"
+var tableAuth = "authorizations"
+var loginIdCol = "loginId"
+var userIdCol = "userId"
+var passwordCol = "password"
 
 type User struct {
-	Id        int       `json:"id"`
-	Name      string    `json:"name"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
-	DeletedAt time.Time `json:"deletedAt"`
+	LoginId  int    `json:"loginId"`
+	Password string `json:"password"`
 }
 
 func (user *User) Create() error {
-	statement := fmt.Sprintf("INSERT INTO %s (%s, %s) VALUES (?, ?)", tableName, idCol, nameCol)
-	stmt, err := mydb.Prepare(statement)
+	// create user table
+	statement := fmt.Sprintf("INSERT INTO %s (%s) VALUES (?)", tableUser, nameCol)
+	prep, err := mydb.Prepare(statement)
 	if err != nil {
 		return err
 	}
 
-	defer stmt.Close()
-	_, err = stmt.Exec(user.Id, user.Name)
+	defer prep.Close()
+	// first, we use login id as username
+	row, err := prep.Exec(user.LoginId)
+	if err != nil {
+		return err
+	}
+	id, err := row.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	// create auth table
+	statement = fmt.Sprintf("INSERT INTO %s (%s, %s, %s) VALUES (?, ?, ?)", tableAuth, loginIdCol, userIdCol, passwordCol)
+	prep, err = mydb.Prepare(statement)
+	if err != nil {
+		return err
+	}
+
+	defer prep.Close()
+	_, err = prep.Exec(user.LoginId, id, user.Password)
 	if err != nil {
 		return err
 	}
