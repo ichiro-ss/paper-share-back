b73d05b0dcfa7f8d83be5fc2dec75cfc82097559
ichiro-ss <sei.swmr@gmail.com>
2024-02-07 12:26:21 +0900

    generate token

--- api/service/user.go
+++ api/service/user.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 var tableUser = "user"
@@ -17,36 +20,51 @@ type UserService struct {
 	db *sql.DB
 }
 
-func (s *UserService) CreateUser(ctx context.Context, loginId, password string) error {
+func GenerateToken(uid string) string {
+	claims := jwt.MapClaims{
+		"user_id": uid,
+		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	// generate payload and header
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	accessToken, _ := token.SignedString([]byte("ACCESS_SECRET_KEY"))
+
+	return accessToken
+}
+
+func (s *UserService) CreateUser(ctx context.Context, loginId, password string) (string, error) {
 	// create user table
 	statement := fmt.Sprintf("INSERT INTO %s (%s) VALUES (?)", tableUser, nameCol)
 	prep, err := s.db.Prepare(statement)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	defer prep.Close()
 	// first, we use login id as username
 	row, err := prep.ExecContext(ctx, loginId)
 	if err != nil {
-		return err
+		return "", err
 	}
 	id, err := row.LastInsertId()
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	// create auth table
 	statement = fmt.Sprintf("INSERT INTO %s (%s, %s, %s) VALUES (?, ?, ?)", tableAuth, loginIdCol, userIdCol, passwordCol)
 	prep, err = s.db.Prepare(statement)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	defer prep.Close()
 	_, err = prep.ExecContext(ctx, loginId, id, password)
 	if err != nil {
-		return err
+		return "", err
 	}
-	return nil
+	token := GenerateToken(loginId)
+	return token, nil
 }
