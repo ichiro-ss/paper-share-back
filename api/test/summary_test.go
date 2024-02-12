511bb9b552b040b5e97cfe6e7639a1073c96ce60
ichiro-ss <sei.swmr@gmail.com>
2024-02-12 22:22:17 +0900

    added edit summary test

--- api/test/summary_test.go
+++ api/test/summary_test.go
@@ -41,6 +41,7 @@ func TestSummary(t *testing.T) {
 		}
 		fmt.Println("status code:", res.StatusCode)
 	})
+
 	t.Run("reading summary", func(t *testing.T) {
 		tkStr := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDc4MTA4ODIsInVzZXJfaWQiOjE3fQ.uuswOlREnkOL7GD4pyRt5Deg-klNrvH9hMIC_l92P4w"
 		client := &http.Client{}
@@ -63,6 +64,41 @@ func TestSummary(t *testing.T) {
 
 		body, err := io.ReadAll(res.Body)
 		if err != nil {
+			t.Error(err)
+		}
+		fmt.Println(string(body))
+	})
+
+	t.Run("edit summary info", func(t *testing.T) {
+		tkStr := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDc4MTA4ODIsInVzZXJfaWQiOjE3fQ.uuswOlREnkOL7GD4pyRt5Deg-klNrvH9hMIC_l92P4w"
+		editSummaryReq := model.EditSummaryRequest{
+			Title:    "edited",
+			Markdown: "## Edited",
+		}
+		SummaryJson, err := json.Marshal(editSummaryReq)
+		if err != nil {
+			t.Error(err)
+		}
+		client := &http.Client{}
+		req, err := http.NewRequest("PUT", "http://localhost:8080/summaries", bytes.NewBuffer(SummaryJson))
+		if err != nil {
+			t.Error(err)
+		}
+		bearer := fmt.Sprintf("Bearer %s", tkStr)
+		req.Header.Set("Authorization", bearer)
+
+		q := req.URL.Query()
+		q.Set("id", "1")
+		req.URL.RawQuery = q.Encode()
+
+		res, err := client.Do(req)
+		if err != nil {
+			t.Error(err)
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil || body == nil {
 			t.Error(err)
 		}
 		fmt.Println(string(body))
