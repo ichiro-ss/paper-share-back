5b16e205c1238031aa93bcd944f9936ce8638522
ichiro-ss <sei.swmr@gmail.com>
2024-02-12 17:00:40 +0900

    create summary service

--- api/service/summary.go
+++ api/service/summary.go
@@ -1,6 +1,14 @@
 package service
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+const tableSummary = "summaries"
+const titleCol = "title"
+const markdownCol = "markdown"
 
 type SummaryService struct {
 	db *sql.DB
@@ -10,4 +18,23 @@ func NewSummaryService(db *sql.DB) *SummaryService {
 	return &SummaryService{
 		db: db,
 	}
+}
+
+func (s *SummaryService) CreateSummary(ctx context.Context, token, title, mk string) error {
+	id, err := TokenToId(token)
+	if err != nil {
+		return err
+	}
+	statement := fmt.Sprintf("INSERT INTO %s (%s, %s, %s) VALUES (?, ?, ?)", tableSummary, userIdCol, titleCol, markdownCol)
+	prep, err := s.db.Prepare(statement)
+	if err != nil {
+		return err
+	}
+	defer prep.Close()
+
+	_, err = prep.ExecContext(ctx, id, title, mk)
+	if err != nil {
+		return err
+	}
+	return nil
 }
