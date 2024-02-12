b8e0f4fef69ff20d41f29bf8044c066baaee0b60
ichiro-ss <sei.swmr@gmail.com>
2024-02-13 01:15:31 +0900

    added delete summary test

--- api/test/summary_test.go
+++ api/test/summary_test.go
@@ -100,4 +100,30 @@ func TestSummary(t *testing.T) {
 		}
 		fmt.Println(string(body))
 	})
+
+	t.Run("delete summary", func(t *testing.T) {
+		client := &http.Client{}
+		req, err := http.NewRequest("DELETE", "http://localhost:8080/summaries", nil)
+		if err != nil {
+			t.Error(err)
+		}
+		bearer := fmt.Sprintf("Bearer %s", tkStr)
+		req.Header.Set("Authorization", bearer)
+
+		q := req.URL.Query()
+		q.Set("id", "2")
+		req.URL.RawQuery = q.Encode()
+
+		res, err := client.Do(req)
+		if err != nil {
+			t.Error(err)
+		}
+		defer res.Body.Close()
+
+		_, err = io.ReadAll(res.Body)
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Println("status code:", res.StatusCode)
+	})
 }
