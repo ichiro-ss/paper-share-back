3f53b643f282423f36b62f37a360f9a41bb3ea9f
ichiro-ss <sei.swmr@gmail.com>
2024-02-12 16:59:59 +0900

    create summary

--- api/handler/summary.go
+++ api/handler/summary.go
@@ -1,8 +1,13 @@
 package handler
 
 import (
+	"api/model"
 	"api/service"
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type SummaryHandler struct {
@@ -16,5 +21,28 @@ func NewSummaryHandler(svc *service.SummaryService) *SummaryHandler {
 }
 
 func (h *SummaryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// POST
+	if r.Method == http.MethodPost {
+		var createSummaryReq model.CreateSummaryRequest
+		if err := json.NewDecoder(r.Body).Decode(&createSummaryReq); err != nil {
+			log.Println(err)
+			return
+		} else {
+			createSummaryReq.Token = strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+			err := h.Create(r.Context(), &createSummaryReq)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+}
 
+func (h *SummaryHandler) Create(ctx context.Context, req *model.CreateSummaryRequest) error {
+	err := h.svc.CreateSummary(ctx, req.Token, req.Title, req.Markdown)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
