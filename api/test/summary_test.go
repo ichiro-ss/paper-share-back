dc64550b5220bc9d56ecaf0d2107fb23adbed6a5
ichiro-ss <sei.swmr@gmail.com>
2024-02-13 14:40:15 +0900

    added get all summaries test

--- api/test/summary_test.go
+++ api/test/summary_test.go
@@ -67,6 +67,28 @@ func TestSummary(t *testing.T) {
 		fmt.Println(string(body))
 	})
 
+	t.Run("reading all summaries", func(t *testing.T) {
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", "http://localhost:8080/summaries", nil)
+		if err != nil {
+			t.Error(err)
+		}
+		bearer := fmt.Sprintf("Bearer %s", tkStr)
+		req.Header.Set("Authorization", bearer)
+
+		res, err := client.Do(req)
+		if err != nil {
+			t.Error(err)
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			t.Error(err)
+		}
+		fmt.Println(string(body))
+	})
+
 	t.Run("edit summary info", func(t *testing.T) {
 		editSummaryReq := model.EditSummaryRequest{
 			Title:    "edited",
@@ -111,7 +133,7 @@ func TestSummary(t *testing.T) {
 		req.Header.Set("Authorization", bearer)
 
 		q := req.URL.Query()
-		q.Set("id", "2")
+		q.Set("id", "14")
 		req.URL.RawQuery = q.Encode()
 
 		res, err := client.Do(req)
