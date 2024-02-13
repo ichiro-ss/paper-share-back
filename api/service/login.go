b88878076c06c78d3a5de82de3cd9bc7cd039580
ichiro-ss <sei.swmr@gmail.com>
2024-02-13 17:21:28 +0900

    added login service

---
+++ api/service/login.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type LoginService struct {
+	db *sql.DB
+}
+
+func NewLoginService(db *sql.DB) *LoginService {
+	return &LoginService{
+		db: db,
+	}
+}
+
+func (s *LoginService) Login(ctx context.Context, loginId, password string) (string, error) {
+	statement := fmt.Sprintf("SELECT %s, %s from %s WHERE loginId = ?", userIdCol, passwordCol, tableAuth)
+	prep, err := s.db.Prepare(statement)
+	if err != nil {
+		return "", err
+	}
+	defer prep.Close()
+
+	var userId int64
+	var passData string
+	err = prep.QueryRowContext(ctx, loginId).Scan(&userId, &passData)
+	if err != nil {
+		return "", err
+	}
+
+	if password != passData {
+		return "", fmt.Errorf("wrong password")
+	}
+	token := GenerateToken(userId)
+	return token, nil
+}
