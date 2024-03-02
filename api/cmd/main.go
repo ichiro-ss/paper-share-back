3e1f52c2c080e368b75ce391bc5f772ea4d95e4f
ichiro-ss <sei.swmr@gmail.com>
2024-03-02 23:59:38 +0900

    graceful shutdown

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
 	"time"
 
 	"api/data"
@@ -20,12 +24,27 @@ func main() {
 	db := data.GetMydb()
 	defer db.Close()
 
+	// graceful shutdown
 	mux := router.NewRouter(db)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
+
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		ctxTimeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		srv.Shutdown(ctxTimeout)
+	}()
+
 	err = srv.ListenAndServe()
+	wg.Wait()
 	if !errors.Is(err, http.ErrServerClosed) {
 		panic(err)
 	}
